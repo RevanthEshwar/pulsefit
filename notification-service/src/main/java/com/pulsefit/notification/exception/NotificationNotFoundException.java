a3ae5b6d631f9992839cd3d63c4ce799bbca827f
V REVANTH ESHWAR <2400032899@kluniversity.in>
2026-09-20 00:27:10 +0530

    Revert "Add notification service"

--- notification-service/src/main/java/com/pulsefit/notification/exception/NotificationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pulsefit.notification.exception;
-
-public class NotificationNotFoundException extends RuntimeException {
-
-    public NotificationNotFoundException(String message) {
-        super(message);
-    }
-}
